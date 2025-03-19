01bb3e5cb51a5caf55e0ea859bd9726fc5438262
Farrel Putra <farrelputradev@gmail.com>
2025-03-19 09:50:12 +0700

    Initial commit

---
+++ src/main/java/com/farrel/lombok/JavaLombokApplication.java
@@ -0,0 +1,13 @@
+package com.farrel.lombok;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaLombokApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaLombokApplication.class, args);
+	}
+
+}
